4dba5e00745e4a7b26e7284f835fbba454a241de
Ged0oo <mohmmednagy@gmail.com>
2026-06-11 00:11:36 +0300

    solve the conflict between user_id and student_id, and solve role bug

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -36,11 +36,13 @@ class AuthController extends Controller
             'token'      => $token,
             'token_type' => 'Bearer',
             'user'       => [
-                'id'         => $user->id,
-                'name'       => $user->name,
-                'email'      => $user->email,
-                'role'       => $user->getRoleNames()->first(),
-                'expires_at' => $user->expires_at,
+                'id'            => $user->id,
+                'name'          => $user->name,
+                'email'         => $user->email,
+                'role'          => $user->roles->first()?->name ?? $user->role,
+                'student_id'    => $user->student ? $user->student->id : null,
+                'instructor_id' => $user->instructor ? $user->instructor->id : null,
+                'expires_at'    => $user->expires_at,
             ],
         ]);
     }
